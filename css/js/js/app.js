36d42449ab4b3848bd844952ef48406a15ac9a85
touseefdxb6747-boop <touseefdxb6747@gmail.com>
2026-07-02 16:32:46 +0400

    app.js

--- css/js/js/app.js
+++ css/js/js/app.js
@@ -1,1 +1,35 @@
+const searchIcon = document.querySelector('.fa-search');
+const searchPopup = document.querySelector('.search-popup');
+const closeSearch = document.querySelector('.close-search');
+const mobileToggle = document.querySelector('.mobile-toggle');
+const nav = document.querySelector('nav');
+const navRight = document.querySelector('.nav-right');
 
+searchIcon.addEventListener('click', () => {
+  searchPopup.classList.add('active');
+});
+
+closeSearch.addEventListener('click', () => {
+  searchPopup.classList.remove('active');
+});
+
+mobileToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  navRight.classList.toggle('active');
+});
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 500){
+    backToTop.classList.add('show');
+  }else{
+    backToTop.classList.remove('show');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top:0,
+    behavior:'smooth'
+  });
+});
